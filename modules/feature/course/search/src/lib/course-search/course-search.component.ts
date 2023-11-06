cb4b8096d5d023aa9ac8dcee7bfe54f3449d3e3a
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-11-06 08:07:08 -0300

    feat(course-search): add lib

---
+++ modules/feature/course/search/src/lib/course-search/course-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'treinamento-ergon-course-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-search.component.html',
+  styleUrls: ['./course-search.component.scss'],
+})
+export class CourseSearchComponent {}
