6fc7fee7f8ff4ce0e65da9fdada518ae46179f5d
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-11-07 17:17:22 -0300

    feat(layout): integrates subheader

--- modules/feature/layout/src/lib/layout.module.ts
+++ modules/feature/layout/src/lib/layout.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SubheaderComponent } from './subheader/subheader.component';
 
 @NgModule({
   imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, SubheaderComponent],
+  exports: [HeaderComponent, SubheaderComponent],
 })
 export class LayoutModule {}
