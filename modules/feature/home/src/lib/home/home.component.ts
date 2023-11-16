891ba158a67a4fa57ea8da609b4f8ff0964ae788
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-11-16 14:55:29 -0300

    feat(home): add home component

---
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'treinamento-ergon-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
