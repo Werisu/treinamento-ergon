faac0d940771784d18e8a4af07fd2fa5bd21f4fb
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-12-13 10:17:33 -0300

    feaat(course-detail): add lib

---
+++ modules/feature/course/detail/src/lib/course-detail/course-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'treinamento-ergon-course-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-detail.component.html',
+  styleUrl: './course-detail.component.scss',
+})
+export class CourseDetailComponent {}
---
+++ modules/feature/course/detail/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+
+export const courseDetailRoutes: Route[] = [
+  { path: '', component: CourseDetailComponent },
+];
