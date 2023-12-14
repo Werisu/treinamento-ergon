15a32170cadf0f525bb8e4e3bf02454b34adbf5c
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-12-14 08:45:46 -0300

    feat(course-detail): add mat toolbar

--- modules/feature/course/detail/src/lib/course-detail/course-detail.component.spec.ts
+++ modules/feature/course/detail/src/lib/course-detail/course-detail.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseDetailComponent } from './course-detail.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CourseDetailComponent', () => {
   let component: CourseDetailComponent;
@@ -8,7 +13,15 @@ describe('CourseDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CourseDetailComponent, RouterTestingModule],
+      imports: [
+        CourseDetailComponent,
+        RouterTestingModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        BrowserAnimationsModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CourseDetailComponent);
--- modules/feature/course/detail/src/lib/course-detail/course-detail.component.ts
+++ modules/feature/course/detail/src/lib/course-detail/course-detail.component.ts
@@ -1,19 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { Observable, map } from 'rxjs';
-
-function getParamId(): Observable<string> {
-  return inject(ActivatedRoute).params.pipe(map((params) => params['id']));
-}
+import { getParamId } from '@treinamento-ergon/course-data-access';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'treinamento-ergon-course-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   templateUrl: './course-detail.component.html',
   styleUrl: './course-detail.component.scss',
 })
 export class CourseDetailComponent {
   public courseId$ = getParamId();
+  public showFiller = false;
+  public titulo = 'Diário Escolar - Treinamento completo';
 }
